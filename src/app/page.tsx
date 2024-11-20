3cd99930041a0a971259713884cb5bad40f5c31d
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-20 18:41:43 +0530

    added daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      hello
+      <Button className="btn btn-outline">Hi</Button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
 
 export default {
     darkMode: ["class"],
+	daisyui: {
+		themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+		darkTheme: "dark", // name of one of the included themes for dark mode
+		base: true, // applies background color and foreground color for root element by default
+		styled: true, // include daisyUI colors and design decisions for all components
+		utils: true, // adds responsive and modifier utility classes
+		prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+		themeRoot: ":root", // The element that receives theme color CSS variables
+	  },
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -20,5 +32,7 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),
+	daisyui,
+  ],
 } satisfies Config;
